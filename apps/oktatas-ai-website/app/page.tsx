0e70f82d023e3575795d6003ac4844b86019961a
Daniel Bacsur <contact@danielbacsur.com>
2024-09-28 17:14:38 +0200

    feat: add landing page prototype for the website

--- apps/oktatas-ai-website/app/page.tsx
+++ apps/oktatas-ai-website/app/page.tsx
@@ -1,101 +1,216 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+import { Balancer } from "react-wrap-balancer";
+import { motion, AnimatePresence } from "framer-motion";
+
+const problems = [
+  "Problémáid vannak az algebrával?",
+  "Segítség kell a történelem esszédhez?",
+  "Nem tudsz rájönni a kémiai feladatra?",
+  "Gondjaid vannak Shakespeare-rel?",
+];
 
 export default function LandingPage() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [problem, setProblem] = useState(problems[0]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProblem((prevProblem) => {
+        const currentIndex = problems.indexOf(prevProblem);
+        const nextIndex = (currentIndex + 1) % problems.length;
+        return problems[nextIndex];
+      });
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <main>
+        <section
+          id="hero"
+          className="bg-gradient-to-b from-gray-100 to-gray-200 pt-24"
+        >
+          <div className="container mx-auto px-6 pt-8">
+            <div className="flex flex-col md:flex-row items-center justify-between">
+              <div className="flex-1 md:pr-8 mb-8 md:mb-0">
+                <motion.h1
+                  className="text-2xl md:text-3xl lg:text-5xl font-bold text-gray-900 text-center md:text-left leading-tight"
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  Ismerd meg Sophiet,
+                  <br />A Virtuális Tanárodat
+                </motion.h1>
+                <motion.p
+                  className="mt-8 text-gray-800 text-center md:text-left leading-relaxed"
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5, delay: 0.2 }}
+                >
+                  Sophie egy AI alapú virtuális magántanár, aki a magyar
+                  követelményeknek megfelelően képes 20 perces személyre szabott
+                  tanórákat tartani.
+                </motion.p>
+                <motion.div
+                  className="mt-8 text-center md:text-left"
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5, delay: 0.4 }}
+                >
+                  <Button className="px-8 md:px-10 py-4 md:py-5 font-semibold rounded-full bg-indigo-600 text-white hover:bg-indigo-700">
+                    Beszélj Sophieval!
+                  </Button>
+                </motion.div>
+              </div>
+              <div className="flex-1 h-80 md:h-[32rem] lg:h-[70dvh]">
+                <motion.img
+                  className="h-full w-full object-contain"
+                  src="/sophie.gif"
+                  alt="Sophie"
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 0.3 }}
+                />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="bg-white py-16 md:py-20">
+          <div className="container max-w-4xl mx-auto px-6">
+            <h2 className="text-3xl font-bold text-gray-900 mb-12 md:mb-16 text-center">
+              Már ennyien tanulnak Sophieval
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-16">
+              <motion.div
+                className="text-center"
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5, delay: 0.2 }}
+              >
+                <p className="text-6xl font-bold text-indigo-600 mb-4 md:mb-6">
+                  13+
+                </p>
+                <p className="text-lg text-gray-800">Tantárgy</p>
+              </motion.div>
+              <motion.div
+                className="text-center"
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5, delay: 0.4 }}
+              >
+                <p className="text-6xl font-bold text-indigo-600 mb-4 md:mb-6">
+                  5000+
+                </p>
+                <p className="text-lg text-gray-800">Segített Diák</p>
+              </motion.div>
+
+              <motion.div
+                className="text-center"
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5, delay: 0.6 }}
+              >
+                <p className="text-6xl font-bold text-indigo-600 mb-4 md:mb-6">
+                  0-24
+                </p>
+                <p className="text-lg text-gray-800">Elérhetőség</p>
+              </motion.div>
+            </div>
+            <div className="mt-12 text-center">
+              <Button className="px-8 md:px-10 py-4 md:py-5 font-semibold rounded-full bg-indigo-600 text-white hover:bg-indigo-700">
+                Próbáld ki te is!
+              </Button>
+            </div>
+          </div>
+        </section>
+
+        <section className="bg-gradient-to-r from-gray-100 to-gray-200 py-16 md:py-20">
+          <div className="container max-w-4xl mx-auto px-6 text-center">
+            <AnimatePresence mode="wait">
+              <motion.h2
+                key={problem}
+                className="text-3xl font-bold text-gray-900 mb-6 md:mb-8"
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{ duration: 0.5 }}
+              >
+                {problem}
+              </motion.h2>
+            </AnimatePresence>
+            <p className="text-gray-800 leading-relaxed">
+              <Balancer>
+                Sophie itt van, hogy segítsen leküzdeni a kihívásaidat személyre
+                szabott órákkal és támogatással.
+              </Balancer>
+            </p>
+
+            <p className="text-gray-800 mb-16 leading-relaxed text-justify">
+              <br />
+              Elsődleges célunk a projekt során az volt, hogy avatárjaink
+              képesek legyenek élethű kommunikációs élményt biztosítani a
+              tanulók számára. Ezáltal a diákok szóban fogalmazhatják meg
+              kérdéseiket, amiket ezek a virtuális tanárok értelmeznek, majd
+              szóban válaszolnak. Csak mint egy ember.
+              <br />
+              <br />
+              Az általunk fejlesztett oktatási platformon úgy néz ki a tanítás,
+              hogy a tanulók a bejelentkezést követően meg tudják szabni, hogy
+              hányadikosok, hogy mit szeretnének megtanulni, és hogy azt milyen
+              szinten. Ezt követően saját AI modelljeink összeállítanak egy
+              egyéni tanrendet a nemzeti alaptantervben megfogalmazott
+              elvárásoknak megfelelően.
+            </p>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-16">
+              <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5 }}
+              >
+                <h3 className="text-xl font-semibold text-indigo-600 mb-6">
+                  Személyre Szabott Órák
+                </h3>
+                <p className="text-gray-800 leading-relaxed text-justify">
+                  Sophie teljes 20 perces órákat kínál, amelyek az egyéni
+                  tanulási igényeidhez igazodnak. Az anyagot a fejlődésed
+                  alapján alakítja, így biztos lehetsz benne, hogy minden
+                  fogalmat megértesz, mielőtt továbblépsz.
+                </p>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: true }}
+                transition={{ duration: 0.5, delay: 0.2 }}
+              >
+                <h3 className="text-xl font-semibold text-indigo-600 mb-6">
+                  Interaktív Tanulási Élmény
+                </h3>
+                <p className="text-gray-800 leading-relaxed text-justify">
+                  Vegyél részt interaktív órákon, amelyek szórakoztatóvá teszik
+                  a tanulást. Sophie játékosan fog bevezetni a tananyagba, és
+                  majd különböző feladatokkal és kvízekkel teszteli a tudásod.
+                </p>
+              </motion.div>
+            </div>
+            <div className="mt-12 text-center">
+              <Button className="px-8 md:px-10 py-4 md:py-5 font-semibold rounded-full bg-indigo-600 text-white hover:bg-indigo-700">
+                Kezdj Tanulni Sophieval!
+              </Button>
+            </div>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
